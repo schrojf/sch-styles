887921c5a0dad5bba5dbb26b74f24d133d1874c0
Viliam Schrojf <viliam@schrojf.sk>
2024-01-17 20:48:51 +0100

    Install Tailwind CSS v3 (PostCSS)

---
+++ src/stories/components/buttons.stories.ts
@@ -0,0 +1,18 @@
+import type {Meta} from "@storybook/html";
+
+const meta = {
+    title: 'Components/Buttons',
+    // render: () => createPage(),
+    parameters: {
+        // More on how to position stories at: https://storybook.js.org/docs/configure/story-layout
+        layout: 'fullscreen',
+    },
+} satisfies Meta;
+
+export default meta;
+
+export const Simple = () => `
+    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+      Button
+    </button>
+`;
